4054161f6a2de4ef14bd926136097594b414505f
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-09-16 00:06:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_db_instance" "hashiconf_rds" {
   engine              = "postgres"
   instance_class      = "db.t3.micro"
   allocated_storage   = 20
-  username            = "admin"
+  username            = "dbadmin"
   skip_final_snapshot = true
 
   password_wo         = ephemeral.aws_secretsmanager_secret_version.hashiconf_readback.secret_string
